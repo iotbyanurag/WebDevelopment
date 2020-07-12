f0fff45fdd1dc62af0864ae09bf5625d2bd78830
anurag tiwari <iotbyanurag@gmail.com>
2020-07-12 11:57:28 +0530

    Added reset functionality to the Game

--- WebDev_Projects/colorGame/colorGame.js
+++ WebDev_Projects/colorGame/colorGame.js
@@ -11,20 +11,38 @@
 // array is populated
 var ColorsArray = [];
 generateRandomColors_Array(6);
-for(var i=0; i< 6; i++){
-    console.log(ColorsArray[i]);
-}
+var pickedcolor = pickColor();
+// for(var i=0; i< 6; i++){
+//     console.log(ColorsArray[i]);
+// }
 var h1 = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
-
-// any random color is picked.
-var pickedcolor = pickColor();
-
-//console.log("picked color" +pickedcolor);
+var resetButton = document.getElementById("reset");
 var colorDisplay= document.getElementById("colorDisplay");
 
+resetButton.addEventListener("click", function(){
+    ColorsArray = [];
+    generateRandomColors_Array(6);
+    pickedcolor = pickColor();
+    colorDisplay.textContent = pickedcolor;
+    h1.style.background = "#232323"
+    for (var i=0; i<squares.length; i++){
+        //select color and apply to the squares
+        squares[i].style.backgroundColor = ColorsArray[i];
+    }
+    // alert("connected");
+}
+);
+
+
+// any random color is picked.
+
+
+//console.log("picked color" +pickedcolor);
+
+
 var Message = document.getElementById("message");
-console.log("picked color" + pickedcolor);
+//console.log("picked color" + pickedcolor);
 colorDisplay.textContent = pickedcolor;
 
 // colors of each tile are populated 
@@ -37,25 +55,42 @@ for (var i=0; i<squares.length; i++){
   
 }
 
+
+function onChangeColor(){
+    // ColorsArray = [];
+    // generateRandomColors_Array(6);
+    // for (var i=0; i<squares.length; i++){
+    //     //select color and apply to the squares
+    //      squares[i].style.backgroundColor = ColorsArray[i];
+    //     // for (var i=0; i< ColorsArray.length; i++){
+    //     //     console.log(ColorsArray[i]);
+    //     // }
+    // }
+    alert("connected");
+}
+
+
 // using this keyword you can refer to the item that was clicked 
 // passed the event handler to the event listener.
 
  function select_Square(){
    // alert(this.style.backgroundColor);
     var clickedColor = this.style.backgroundColor;
-    console.log("clicked" + clickedColor);
+    //console.log("clicked" + clickedColor);
     if(clickedColor === pickedcolor){
         // alert("CORRECT");
         Message.textContent = "Correct";
         changeColorOnMatch(clickedColor);
         h1.style.background = clickedColor;
+        resetButton.textContent = "PLAY AGAIN?";
     }
     else{
        this.style.background = "#232323";
        Message.textContent = "Try Again";   
        //alert("Wrong"); 
+       resetButton.textContent = "New Colors";
     } 
-}
+ }
 
 function changeColorOnMatch(color){
     // on getting correct answer, change color of all tiles to the color of correct suare
